5dcd8a5cab3e69da324672230e7cd3d1bcf4f6a4
harishsingh-01 <harishchaudhary790@gmail.com>
2025-03-27 20:10:20 +0530

    images update

--- src/pages/UpdateRoom.js
+++ src/pages/UpdateRoom.js
@@ -18,7 +18,7 @@ const UpdateRoom = () => {
         const token = localStorage.getItem("token");
  
         const response = await API.get(`/api/rooms/${roomId}`, {
-          headers: { Authorization: `${token}` },
+          headers: { Authorization: `Bearer ${token}` },
         });
 
  
@@ -186,7 +186,7 @@ const UpdateRoom = () => {
 
             {/* Price */}
             <div className="space-y-1">
-              <label className="block text-sm font-medium text-gray-700">Price per Month</label>
+              <label className="block text-sm font-medium text-gray-700">Price per Night</label>
               <div className="relative rounded-md shadow-sm">
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   <DollarSign className="h-5 w-5 text-gray-400" />
