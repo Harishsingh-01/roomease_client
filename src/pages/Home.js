403d339dc12a1f657e00861f153f5e14fc752adc
harishsingh-01 <harishchaudhary790@gmail.com>
2025-04-13 08:54:47 +0530

    changes related to images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
-import roomImage from "./download.jpg";
 import backgroundImage from "./vojtech-bruzek-Yrxr3bsPdS0-unsplash.jpg";
 import API from "../utils/axiosInstance";
 
