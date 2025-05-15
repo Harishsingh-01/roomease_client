f4ec2fbf33dd1a4e14b416ff47450450dc38fed4
harishsingh-01 <harishchaudhary790@gmail.com>
2025-05-15 12:59:53 +0530

    xml change

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://roomeaseserver.onrender.com", 
+  baseURL: "http://localhost:5000", 
   headers: {
     "Content-Type": "application/json",
   },
